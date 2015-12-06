a0decc1ef3cd3bf4db42dd04ce16b34512f1c667
Redwater Soccer <redwatersoccer@gmail.com>
2015-12-06 14:14:55 -0700

    toggle drawer is not working in dist

--- app/app.js
+++ app/app.js
@@ -1,19 +1,31 @@
 (function(document) {
   'use strict';
-  
+
   var app = document.querySelector('#app');
 
   app.addEventListener('dom-change', function() {
-
-  });
+    console.log('dom-change');
+      });
 
   window.addEventListener('WebComponentsReady', function() {
+ console.log('web components ready'); 
 
+  var mb = document.getElementById('menubutton');
+    mb.onclick = function() {
+      console.log('mbclick');
+      var adl = document.getElementById('app-drawer-layout');
+      adl.opened = !adl.opened;
+    };
+
+  
+  
   });
 
   app.onDataRouteClick = function() {
     var adl = document.getElementById('app-drawer-layout');
-    adl.toggleDrawer();
+    console.log(adl);
+    // adl.toggleDrawer();
+    adl.opened = !adl.opened;
   };
 
   app.scrollPageToTop = function() {
